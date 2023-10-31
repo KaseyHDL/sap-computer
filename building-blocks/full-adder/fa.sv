f39a5b766266b1ad4afd86bc18c36ad3140da60d
KaseyHDL <kasey.fpga@gmail.com>
2023-10-30 22:19:00 -0400

    updated readme

--- building-blocks/full-adder/fa.sv
+++ building-blocks/full-adder/fa.sv
@@ -20,19 +20,8 @@ module fa (
   logic sum_ha_1;
   logic carry_ha_1;  
 
-  ha ha_0 (
-    .A     (A),
-    .B     (sum_ha_1),
-    .SUM   (SUM),
-    .CARRY (carry_ha_0)
-  );
-
-  ha ha_1 (
-    .A     (B),
-    .B     (C),
-    .SUM   (sum_ha_1),
-    .CARRY (carry_ha_1)
-  );
+  ha ha_0 (.A (A), .B (sum_ha_1), .SUM (SUM),      .CARRY (carry_ha_0));
+  ha ha_1 (.A (B), .B (C),        .SUM (sum_ha_1), .CARRY (carry_ha_1));
 
   or or_0 (CARRY, carry_ha_0, carry_ha_1);
 
